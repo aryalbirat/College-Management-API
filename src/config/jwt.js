3c36a1888422ea78fa6b44274d54266477b6fc85
Birat <itsbirataryal+github@gmail.com>
2025-07-09 18:39:17 +0545

    Jwt file updated

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+//JWT token genereation 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE || '1d'
